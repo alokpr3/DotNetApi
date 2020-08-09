d7e6d57329722d665f71253a1ba754aed06abd88
Alok Gupta <bob@gmail.com>
2020-08-09 09:21:33 +0530

    first commit

---
+++ DotNetApi/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DotNetApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] MathRequest request)
+        {
+            try
+            {
+                var result = request.FirstVal + request.SecondVal;
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        public class MathRequest
+        {
+            public int FirstVal { get; set; }
+            public int SecondVal { get; set; }
+        }
+    }
+
+
+}
